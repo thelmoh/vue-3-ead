8b1d6173ea922dca8148c07f94f3765e71588cc0
Thelmo Severo <thelmosevero@gmail.com>
2023-06-09 21:50:01 -0300

    AULA 2 MÓD 2 - CRIAÇÃO DE PÁG DE TEMPLATE

---
+++ src/layouts/components/FooterComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer id="footer">
+        <span class="copyright fontSmall">
+            Todos os Direitos reservados - <b>Especializati</b>
+        </span>
+    </footer>
+</template>
+
+<script>
+    export default {
+        name: 'footer-component'    
+    }
+</script>
---
+++ src/views/supports/MySupports.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="home">
+      Minhas Dúvidas
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MySupports'
+  }
+  </script>
+  
